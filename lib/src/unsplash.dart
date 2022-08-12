30736a18b7e77d1535905925986a3b96f3045388
lamphuchai <lamphuchai38@gmail.com>
2022-08-13 00:54:22 +0700

    up

--- lib/src/unsplash.dart
+++ lib/src/unsplash.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:unsplash_dart/src/api/api.dart';
 import 'package:unsplash_dart/src/api/current_user.dart';
+import 'package:unsplash_dart/src/exceptions/unsplash_exception.dart';
 import 'package:unsplash_dart/src/network/dio_client.dart';
 
 class Unsplash {
@@ -11,8 +13,10 @@ class Unsplash {
   late final Search _search;
   late final Topics _topics;
   late final Stats _stats;
+  late final String? _clientId;
 
   void createApi(String clientId) {
+    _clientId = clientId;
     final dioClient = DioClient(clientId);
     _auth = Auth(dioClient);
     _currentUser = CurrentUser(dioClient);
@@ -32,4 +36,21 @@ class Unsplash {
   Search get search => _search;
   Topics get topics => _topics;
   Stats get stats => _stats;
+
+  Future<Map<String, dynamic>> clientRateLimit() async {
+    if (_clientId == null) {
+      throw const UnsplashException(message: "Do not clientId");
+    }
+    try {
+      final data = await Dio().get("https://api.unsplash.com/photos/random",
+          queryParameters: {"client_id": _clientId});
+      final rateLimit = {
+        "limit": data.headers.value("X-Ratelimit-Limit"),
+        "remaining": data.headers.value("X-Ratelimit-Remaining")
+      };
+      return rateLimit;
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
