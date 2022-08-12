c886ff08b50b13491666c6f05ff623d9b7df651a
lamphuchai <lamphuchai38@gmail.com>
2022-08-12 08:26:42 +0700

    init

---
+++ lib/src/constants/enum.dart
@@ -0,0 +1,30 @@
+enum OrderBy { latest, oldest, popular, views, downloads }
+
+enum LikesPhotosOrderBy { latest, oldest, popular }
+
+enum PhotosOrderBy { latest, oldest, popular }
+
+enum OrderBySearch { latest, relevant }
+
+enum OrderByTopic { featured, latest, oldest, position }
+
+enum TopicPhotosOrderBy { latest, oldest, popular }
+
+enum Orientation { landscape, portrait, squarish }
+
+enum ContentFilter { low, high }
+
+enum ColorUnsplash {
+  // ignore: constant_identifier_names
+  black_and_white,
+  black,
+  white,
+  yellow,
+  orange,
+  red,
+  purple,
+  magenta,
+  green,
+  teal,
+  blue
+}
---
+++ lib/src/models/exif.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+class Exif {
+  const Exif(
+      {this.make,
+      this.model,
+      this.exposureTime,
+      this.aperture,
+      this.focalLength,
+      this.isoSpeedEatings});
+  final String? make;
+  final String? model;
+  final String? exposureTime;
+  final String? aperture;
+  final String? focalLength;
+  final String? isoSpeedEatings;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'make': make,
+      'model': model,
+      'exposure_time': exposureTime,
+      'aperture_value': aperture,
+      'focal_length': focalLength,
+      'iso_speed_ratings': isoSpeedEatings,
+    };
+  }
+
+  factory Exif.fromMap(Map<String, dynamic> map) {
+    return Exif(
+      make: map['make'] != null ? map['make'] as String : null,
+      model: map['model'] != null ? map['model'] as String : null,
+      exposureTime:
+          map['exposure_time'] != null ? map['exposure_time'] as String : null,
+      aperture: map['aperture'] != null ? map['aperture'] as String : null,
+      focalLength:
+          map['focal_length'] != null ? map['focal_length'] as String : null,
+      isoSpeedEatings: map['iso'] != null ? '${map['iso']}' : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Exif.fromJson(String source) =>
+      Exif.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Exif(make: $make, model: $model, exposureTime: $exposureTime, aperture: $aperture, focalLength: $focalLength, isoSpeedEatings: $isoSpeedEatings)';
+  }
+}
