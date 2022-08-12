186b22782909b442a078df8f86af7e0f3aa21329
lamphuchai <lamphuchai38@gmail.com>
2022-08-12 22:40:50 +0700

    up

--- lib/src/models/links.dart
+++ lib/src/models/links.dart
@@ -7,14 +7,19 @@ class Links {
   final String? downloadLocation;
   final String? related;
   final String? photos;
-  Links({
-    required this.self,
-    required this.html,
-    required this.download,
-    this.downloadLocation,
-    this.related,
-    this.photos,
-  });
+  String? portfolio;
+  String? following;
+  String? followers;
+  Links(
+      {required this.self,
+      required this.html,
+      required this.download,
+      this.downloadLocation,
+      this.related,
+      this.photos,
+      this.portfolio,
+      this.following,
+      this.followers});
 
   Links copyWith({
     String? self,
@@ -23,15 +28,20 @@ class Links {
     String? downloadLocation,
     String? related,
     String? photos,
+    String? portfolio,
+    String? following,
+    String? followers,
   }) {
     return Links(
-      self: self ?? this.self,
-      html: html ?? this.html,
-      download: download ?? this.download,
-      downloadLocation: downloadLocation ?? this.downloadLocation,
-      related: related ?? this.related,
-      photos: photos ?? this.photos,
-    );
+        self: self ?? this.self,
+        html: html ?? this.html,
+        download: download ?? this.download,
+        downloadLocation: downloadLocation ?? this.downloadLocation,
+        related: related ?? this.related,
+        photos: photos ?? this.photos,
+        portfolio: portfolio ?? this.portfolio,
+        following: following ?? this.following,
+        followers: followers ?? this.followers);
   }
 
   Map<String, dynamic> toMap() {
@@ -42,17 +52,23 @@ class Links {
       'download_location': downloadLocation,
       'related': related,
       'photos': photos,
+      'portfolio': portfolio,
+      'following': following,
+      'followers': followers,
     };
   }
 
   factory Links.fromMap(Map<String, dynamic> map) {
     return Links(
-      self: map['self'] ?? '',
-      html: map['html'] ?? '',
-      download: map['download'] ?? '',
+      self: map['self'] ?? 'https://unsplash.com/',
+      html: map['html'] ?? 'https://unsplash.com/',
+      download: map['download'] ?? 'https://unsplash.com/',
       downloadLocation: map['download_location'],
       related: map['related'],
       photos: map['photos'],
+      portfolio: map['portfolio'],
+      following: map['following'],
+      followers: map['followers'],
     );
   }
 
@@ -63,28 +79,5 @@ class Links {
   @override
   String toString() {
     return 'Links(self: $self, html: $html, download: $download, downloadLocation: $downloadLocation, related: $related, photos: $photos)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Links &&
-        other.self == self &&
-        other.html == html &&
-        other.download == download &&
-        other.downloadLocation == downloadLocation &&
-        other.related == related &&
-        other.photos == photos;
-  }
-
-  @override
-  int get hashCode {
-    return self.hashCode ^
-        html.hashCode ^
-        download.hashCode ^
-        downloadLocation.hashCode ^
-        related.hashCode ^
-        photos.hashCode;
   }
 }
